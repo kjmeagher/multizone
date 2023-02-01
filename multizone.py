e36f3fcb2c811f7b0ede7b704d6164805d0dd3fb
Kevin Meagher <11620178+kjmeagher@users.noreply.github.com>
2023-02-01 14:35:37 -0600

    Initial Commit

---
+++ multizone.py
@@ -0,0 +1,252 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: 2023 Kevin Meagher
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+"""Command line tool for calculating times in multiple different timezones."""
+
+__version__ = "0.1.0"
+
+
+import argparse
+import sys
+from datetime import datetime, timedelta, tzinfo
+from pathlib import Path
+from typing import Any, Dict, List, Optional, Tuple
+
+import tabulate
+import tzlocal
+import xdg
+from babel.core import default_locale
+from babel.dates import format_datetime
+from termcolor import colored
+
+if sys.version_info < (3, 9):
+    from backports import zoneinfo
+else:
+    import zoneinfo
+if sys.version_info < (3, 11):
+    import tomli as tomllib
+else:
+    import tomllib
+
+ConfigType = Dict[str, Any]
+
+
+def get_arg(time_str: str, sep: str) -> List[int]:
+    """Parse date or time with 1 or two delimiters."""
+    split = time_str.split(sep)
+    if len(split) in (2, 3):
+        return [int(x) for x in split]
+    raise ValueError
+
+
+def parse_time_arg(time_arg: List[str], default_time: datetime, default_zone: tzinfo) -> datetime:
+    """Parse the date time an time zone arguments."""
+    refdate = None
+    reftime = None
+    refzone: Optional[tzinfo] = None
+
+    for arg in time_arg:
+        if refdate is None:
+            try:
+                refdate = get_arg(arg, "-")
+                continue
+            except ValueError:
+                pass
+            try:
+                refdate = get_arg(arg, "/")
+                continue
+            except ValueError:
+                pass
+        if reftime is None:
+            try:
+                reftime = get_arg(arg, ":")
+                continue
+            except ValueError:
+                pass
+        if refzone is None:
+            try:
+                refzone = zoneinfo.ZoneInfo(arg)
+                continue
+            except zoneinfo.ZoneInfoNotFoundError:
+                pass
+        mesg = f'I don\'t understand argument "{arg}"'
+        raise ValueError(mesg)
+
+    if not refzone:
+        refzone = default_zone
+    ref_dt = default_time.astimezone(refzone)
+    if refdate:
+        if len(refdate) == 2:
+            refdate.insert(0, ref_dt.year)
+    else:
+        refdate = [ref_dt.year, ref_dt.month, ref_dt.day]
+
+    if reftime:
+        if len(reftime) == 2:
+            reftime.append(0)
+    else:
+        reftime = [ref_dt.hour, ref_dt.minute, ref_dt.second]
+
+    return datetime(refdate[0], refdate[1], refdate[2], reftime[0], reftime[1], reftime[2], tzinfo=refzone)
+
+
+def parse_args() -> argparse.Namespace:
+    """Parse command line arguments with argparse."""
+    parser = argparse.ArgumentParser(description="Convert localtime to other timezones")
+    parser.add_argument("time", nargs="*", help="Time to convert (Default: Now)")
+    parser.add_argument("--config", help="Configuration file")
+    parser.add_argument(
+        "--format",
+        "-f",
+        help="Output format to use for times, same as used by strftime",
+    )
+    parser.add_argument("--zones", "-z", nargs="+", help="List of timezones to print out that time")
+    parser.add_argument("--list", action="store_true", help="Print a list of all timezones and exit")
+    parser.add_argument("-l", "--locale")
+    args = parser.parse_args()
+
+    if args.list:
+        for avail_zones in sorted(zoneinfo.available_timezones()):
+            print(avail_zones)  # noqa: T201
+        sys.exit(0)
+    return args
+
+
+def load_config(config_path: Optional[Path]) -> ConfigType:
+    """Read toml configuration file from given path or default path."""
+    if config_path is None:
+        config_filename = xdg.xdg_config_home() / "multizone" / "multizone.toml"
+        if not config_filename.is_file():
+            config_filename = None
+    else:
+        config_filename = Path(config_path)
+    if config_filename is not None:
+        with config_filename.open("rb") as file_buff:
+            return tomllib.load(file_buff)
+    else:
+        return {}
+
+
+def configure(args: argparse.Namespace, config_file: ConfigType) -> ConfigType:
+    """Get the configuration from the command line arguments and contents of config file."""
+    config = {}
+
+    if args.format:
+        config["format"] = args.format
+    else:
+        config["format"] = config_file.get("format", "E, dd MMM, yyyy 'at' HH:mm a Z")
+
+    if args.zones:
+        config["zones"] = args.zones
+    else:
+        config["zones"] = config_file.get("zones", [])
+
+    config["aliases"] = config_file.get("aliases", {})
+
+    config["time"] = args.time
+
+    config["reftime"] = {"color": None, "on_color": None, "attrs": []}
+    config["reftime"].update(config_file.get("reftime", {}))
+
+    config["localtime"] = {"color": None, "on_color": None, "attrs": []}
+    config["localtime"].update(config_file.get("localtime", {}))
+
+    if args.locale:
+        config["locale"] = args.locale
+    else:
+        config["locale"] = config_file.get("locale", default_locale())
+
+    return config
+
+
+def time_offset(item: Tuple[str, datetime]) -> timedelta:
+    """Key function to sort times."""
+    offset = item[1].utcoffset()
+    assert offset is not None
+    return -offset
+
+
+def zone_table(
+    timezones: List[str],
+    aliases: Dict[str, str],
+    refdt: datetime,
+    localzone: tzinfo,
+) -> List[Tuple[str, datetime]]:
+    """Create a table of times for each time zone."""
+    zones: List[Tuple[str, datetime]] = []
+    found_local = False
+    found_ref = False
+    localdt = refdt.astimezone(localzone)
+    localoffset = localdt.utcoffset()
+    refoffset = refdt.utcoffset()
+
+    for zonename in timezones:
+        name = aliases.get(zonename, zonename)
+        zone = zoneinfo.ZoneInfo(zonename)
+        time = refdt.astimezone(zone)
+        if time.utcoffset() == localoffset:
+            found_local = True
+        if time.utcoffset() == refoffset:
+            found_ref = True
+        zones.append((name, time))
+
+    if not found_local:
+        zones.append((str(localzone), localdt))
+
+    if not found_ref and localoffset != refoffset:
+        tzname = refdt.tzname()
+        assert tzname is not None
+        zones.append((tzname, refdt))
+
+    zones.sort(key=time_offset)
+    return zones
+
+
+def table_to_string(
+    zones: List[Tuple[str, datetime]],
+    refdt: datetime,
+    localzone: tzinfo,
+    config: ConfigType,
+) -> str:
+    """Convert a table of times into a tabulated string to print."""
+    zones2 = []
+    for name, time in zones:
+        timestr = format_datetime(time, config["format"], locale=config["locale"])
+        if time.utcoffset() == refdt.astimezone(localzone).utcoffset():
+            name = colored(name, **config["localtime"])
+            timestr = colored(timestr, **config["localtime"])
+        if time.utcoffset() == refdt.utcoffset():
+            name = colored(name, **config["reftime"])
+            timestr = colored(timestr, **config["reftime"])
+        zones2.append((name, timestr))
+
+    return tabulate.tabulate(zones2, tablefmt="plain")
+
+
+def main() -> None:  # pragma: no cover
+    """Script entry point."""
+    # get the current time and the machine's timezone
+    utcnow = datetime.now(tz=zoneinfo.ZoneInfo("UTC"))
+    localzone = tzlocal.get_localzone()
+
+    args = parse_args()
+    config_file = load_config(args.config)
+
+    # run configuration
+    config = configure(args, config_file)
+
+    # parse the time given at command line and localzone
+    refdt = parse_time_arg(config["time"], utcnow, localzone)
+
+    # create the actual table
+    zones = zone_table(config["zones"], config["aliases"], refdt, localzone)
+
+    # print the table
+    print(table_to_string(zones, refdt, localzone, config))  # noqa: T201
+
+
+if __name__ == "__main__":
+    main()  # pragma: no cover
---
+++ test_mz.py
@@ -0,0 +1,200 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: © 2023 Kevin Meagher
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+"""Tests for multizone."""
+
+import contextlib
+import sys
+from argparse import Namespace
+from datetime import datetime
+from pathlib import Path
+from unittest.mock import MagicMock, mock_open, patch
+
+import pytest
+from babel.core import default_locale
+
+import multizone
+
+if sys.version_info < (3, 9):
+    from backports.zoneinfo import ZoneInfo
+else:
+    from zoneinfo import ZoneInfo
+
+
+def test_parse_time_args() -> None:
+    """Tests parse_time_args()."""
+    utc = ZoneInfo("UTC")
+    time0 = datetime(2023, 1, 1, 00, 00, 00, tzinfo=utc)
+
+    # parse the time given at command line and localzone
+    time1 = multizone.parse_time_arg(["3:30"], time0, utc)
+    assert time1.year == time0.year
+    assert time1.month == time0.month
+    assert time1.day == time0.day
+    assert time1.hour == 3
+    assert time1.minute == 30
+    assert time1.tzinfo == time0.tzinfo
+
+    time2 = multizone.parse_time_arg(["4/26"], time0, utc)
+    assert time2.year == time0.year
+    assert time2.month == 4
+    assert time2.day == 26
+    assert time2.hour == time0.hour
+    assert time2.minute == time0.minute
+    assert time2.tzinfo == time0.tzinfo
+
+    time2 = multizone.parse_time_arg(["7-12"], time0, utc)
+    assert time2.year == time0.year
+    assert time2.month == 7
+    assert time2.day == 12
+    assert time2.hour == time0.hour
+    assert time2.minute == time0.minute
+    assert time2.tzinfo == time0.tzinfo
+
+    time3 = multizone.parse_time_arg(["Asia/Tokyo"], time0, utc)
+    assert time3.year == time0.year
+    assert time3.month == time0.month
+    assert time3.day == time0.day
+    assert time3.hour == 9
+    assert time3.minute == time0.minute
+    assert time3.tzinfo == ZoneInfo("Asia/Tokyo")
+
+    with pytest.raises(ValueError, match="ASDF"):
+        multizone.parse_time_arg(["ASDF"], time0, utc)
+
+
+@pytest.mark.skipif(sys.version_info < (3, 9), reason="older versions of mock can't patch sys.argv")
+def test_parse_args() -> None:
+    """Test parse_args()."""
+    with patch("sys.argv", ["multizone", "--list"]), contextlib.redirect_stdout(None):
+        with pytest.raises(SystemExit) as pytest_wrapped_e:
+            multizone.parse_args()
+        assert pytest_wrapped_e.type == SystemExit
+
+    iargs = [
+        "multizone",
+        "7/21",
+        "3:30",
+        "US/Eastern",
+        "--config=asdf.toml",
+        "--zones",
+        "a",
+        "b",
+        "c",
+        "--locale=en_US",
+        "--format",
+        "%H:%M",
+    ]
+    with patch("sys.argv", iargs):
+        args = multizone.parse_args()
+        assert args.time == ["7/21", "3:30", "US/Eastern"]
+        assert args.config == "asdf.toml"
+        assert args.zones == ["a", "b", "c"]
+        assert args.list is False
+        assert args.format == "%H:%M"
+
+
+def test_load_config() -> None:
+    """Test load_config()."""
+    with patch.multiple(
+        "pathlib.Path",
+        open=mock_open(read_data=b""),
+        is_file=MagicMock(return_value=False),
+    ):
+        assert multizone.load_config(None) == {}
+
+    with patch.multiple(
+        "pathlib.Path",
+        open=mock_open(read_data=b"time=1984"),
+        is_file=MagicMock(return_value=True),
+    ):
+        assert multizone.load_config(None) == {"time": 1984}
+
+    with patch("pathlib.Path.open", mock_open(read_data=b"format = '%h:%m'")):
+        assert multizone.load_config(Path("ASDF")) == {"format": "%h:%m"}
+
+
+def test_configure() -> None:
+    """Test configure()."""
+    args1 = Namespace(format=None, zones=None, locale=None, time=None)
+    args2 = Namespace(format="HH:mm", zones=["UTC"], locale="de_DE", time="3:30")
+    cfg = {
+        "format": "H:m",
+        "zones": ["ROK", "ROC"],
+        "aliases": {"ROK": "Korea"},
+        "reftime": {"color": "red"},
+        "localtime": {"color": "green"},
+        "locale": "fr_FR",
+    }
+
+    assert multizone.configure(args1, cfg) == {
+        "format": "H:m",
+        "zones": ["ROK", "ROC"],
+        "aliases": {"ROK": "Korea"},
+        "time": None,
+        "reftime": {"color": "red", "on_color": None, "attrs": []},
+        "localtime": {"color": "green", "on_color": None, "attrs": []},
+        "locale": "fr_FR",
+    }
+    assert multizone.configure(args2, cfg) == {
+        "format": "HH:mm",
+        "zones": ["UTC"],
+        "aliases": {"ROK": "Korea"},
+        "time": "3:30",
+        "reftime": {"color": "red", "on_color": None, "attrs": []},
+        "localtime": {"color": "green", "on_color": None, "attrs": []},
+        "locale": "de_DE",
+    }
+
+    assert multizone.configure(args1, {}) == {
+        "format": "E, dd MMM, yyyy 'at' HH:mm a Z",
+        "zones": [],
+        "aliases": {},
+        "time": None,
+        "reftime": {"color": None, "on_color": None, "attrs": []},
+        "localtime": {"color": None, "on_color": None, "attrs": []},
+        "locale": default_locale(),
+    }
+
+
+def test_zone_table() -> None:
+    """Test zone_table()."""
+    utc = ZoneInfo("UTC")
+    rok = ZoneInfo("ROK")
+    est = ZoneInfo("EST")
+    time0 = datetime(2023, 1, 1, 00, 00, 00, tzinfo=utc)
+    time_rok = time0.astimezone(rok)
+    time_est = time0.astimezone(est)
+    tab1 = multizone.zone_table([], {}, time0, utc)
+    assert tab1 == [("UTC", time0)]
+
+    tab2 = multizone.zone_table([], {}, time0, rok)
+    assert tab2 == [("ROK", time_rok), ("UTC", time0)]
+
+    tab3 = multizone.zone_table(["ROK", "EST", "UTC"], {}, time0, rok)
+    assert tab3 == [("ROK", time_rok), ("UTC", time0), ("EST", time_est)]
+
+    tab4 = multizone.zone_table(["ROK", "EST", "UTC"], {"ROK": "Korea", "EST": "Eastern"}, time0, rok)
+    assert tab4 == [("Korea", time_rok), ("UTC", time0), ("Eastern", time_est)]
+
+
+def test_table_to_string() -> None:
+    """Test table_to_string()."""
+    utc = ZoneInfo("UTC")
+    rok = ZoneInfo("ROK")
+    time0 = datetime(2023, 1, 1, 00, 00, 00, tzinfo=utc)
+    time_rok = time0.astimezone(rok)
+    tttt = [("Korea", time_rok), ("UTC", time0)]
+    config = {"format": "HH:mm", "locale": "en_US", "localtime": {}, "reftime": {}}
+    output = multizone.table_to_string(tttt, time_rok, utc, config).split("\n")
+    assert "Korea" in output[0]
+    assert "09:00" in output[0]
+    assert "UTC" in output[1]
+    assert "00:00" in output[1]
+
+
+if __name__ == "__main__":
+    pytest.main(["test_mz.py"])
